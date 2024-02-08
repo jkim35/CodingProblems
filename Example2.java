ebe3ec84bc88470a037888a5cc22ecb14c351bf1
Team6911 <155586579+jkim35@users.noreply.github.com>
2024-02-08 16:32:05 -0500

    qwertyui

---
+++ Example2.java
@@ -0,0 +1,9 @@
+public class Example2 {
+    public static void main(String[]args){
+
+        System.out.println(" ************************** \n * \t \t \t  * \n * \t \t \t  * \n * \t \t \t  * \n * \t \t \t  *");
+        System.out.println(" *SwimWithSue@swimwear.com*");
+        System.out.println(" * \t \t \t  * \n * \t \t \t  * \n * \t \t \t  * \n * \t \t \t  * \n **************************");
+    }
+    
+}
